55a6b3a9fcbb185133a7e250feadd4d77ce6343c
tarikulislam <tarikulislam4145@gmail.com>
2024-02-21 07:20:37 +0530

    basic

---
+++ 1st/Type_Conversion.js
@@ -0,0 +1,45 @@
+let str = "123";
+let num = Number(str);
+console.log(num); // Output: 123
+let bool = Boolean(str); // true for any non-empty string
+console.log(bool); // Output: true
+let str2 = String(num);
+console.log(str2); // Output: "123"
+
+
+// *********** start  important ***********
+
+
+let name ="tarikul";
+let bool2 = Boolean(name); // true for any non-empty string
+
+console.log(bool2); // Output: true
+
+let name2 = "tarikulX20";
+let num2 = Number(name2);
+console.log(num2); // Output: NaN
+
+
+
+// *********** Operation ***********
+let a = 5;
+let b = 2;
+
+let sum = a + b;
+console.log(sum); // Output: 7
+
+let difference = a - b;
+console.log(difference); // Output: 3
+
+let product = a * b;
+console.log(product); // Output: 10
+
+let quotient = a / b;
+console.log(quotient); // Output: 2.5
+
+let remainder = a % b;
+console.log(remainder); // Output: 1
+
+let exponentiation = a ** b;
+console.log(exponentiation); // Output: 25
+
---
+++ 1st/dataType.js
@@ -0,0 +1,28 @@
+
+// *********** start  important *********** 
+
+let name = "John"; // string
+let age = 25; // number
+let isAdult = true; // boolean
+let city = "New York"; // string
+let x; // undefined
+let y = null; // null
+let z = Symbol("symbol"); // symbol
+let a = { name, age, isAdult, city }; // object
+let b = [name, age, isAdult, city]; // array
+let c = function() { return "hello world"; }; // function
+
+// *********** end  important ***********
+
+let d = /hello/; // regular expression
+let e = new Date(); // date
+let f = new Error("error"); // error
+let g = new Map(); // map
+let h = new Set(); // set
+let i = new WeakMap(); // weak map
+let j = new WeakSet(); // weak set
+let k = new ArrayBuffer(8); // array buffer
+let l = new DataView(k); // data view
+let m = new Promise((resolve, reject) => { resolve("resolved"); }); // promise
+let n = new Int8Array(); // typed array
+
