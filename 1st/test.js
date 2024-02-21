b6b9f67200f61d89250a093ff6f154ee31bcc19e
tarikulislam <tarikulislam4145@gmail.com>
2024-02-21 06:35:03 +0530

    variable

---
+++ 1st/test.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+const name = "John";
+
+let age = 25;
+
+var isAdult = true;
+
+city = "New York";
+console.log(name, age, isAdult);
+
+console.table({ name, age, isAdult ,city });
